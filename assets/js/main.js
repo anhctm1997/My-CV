4f681816d1438f179586172576b0a0d19564a59d
Cuong Le <cuongedward97@gmail.com>
2022-01-30 05:55:13 +0700

    complete 50%

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,49 @@
 /*==================== SHOW MENU ====================*/
-
+const showMenu = (toggleId, navId) =>{
+    const toggle = document.getElementById(toggleId),
+    nav = document.getElementById(navId)
+    
+    // Validate that variables exist
+    if(toggle && nav){
+        toggle.addEventListener('click', ()=>{
+            // We add the show-menu class to the div tag with the nav__menu class
+            nav.classList.toggle('show-menu')
+        })
+    }
+}
+showMenu('nav-toggle','nav-menu')
 
 /*==================== REMOVE MENU MOBILE ====================*/
+// Lấy Element của class .navlink 
+const navLink = document.querySelectorAll('.nav__link')
 
+// khi nhấn vào nav__link thì sẽ remove class show-menu
+function linkAction(){
+    const navMenu = document.getElementById('nav-menu')
+    navMenu.classList.remove('show-menu')
+}
+// Chờ sự kiện click nhấn vào nav__link thì dùng hàm linkAction
+navLink.forEach(n => n.addEventListener('click', linkAction))
 
 /*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
+const sections = document.querySelectorAll('section[id]')
+
+function scrollActive(){
+    const scrollY = window.pageYOffset
+
+    sections.forEach(current =>{
+        const sectionHeight = current.offsetHeight
+        const sectionTop = current.offsetTop - 50;
+        sectionId = current.getAttribute('id')
+
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active-link')
+        }else{
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active-link')
+        }
+    })
+}
+window.addEventListener('scroll', scrollActive)
 
 
 /*==================== SHOW SCROLL TOP ====================*/ 
