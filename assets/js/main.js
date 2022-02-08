c9120ff52656fdeecf314312a669d7a130533dd3
Cuong Le <cuongedward97@gmail.com>
2022-02-08 13:50:29 +0700

    upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -61,8 +61,8 @@ function showAvatarNav() {
     const navAvatar = document.getElementById('nav-avatar');
     // When the scroll is higher than 560 viewport height, add the show-scroll class to the a tag with the scroll-top class
     if (this.scrollY >= 144) {
-        navAvatar.classList.add('show-avatar');
         navName.classList.remove('show-avatar');
+        navAvatar.classList.add('show-avatar');
     } else {
         navAvatar.classList.remove('show-avatar');
         navName.classList.add('show-avatar');
@@ -71,7 +71,34 @@ function showAvatarNav() {
 window.addEventListener('scroll', showAvatarNav)
 
 /*==================== DARK LIGHT THEME ====================*/
+const themeButton = document.getElementById('theme-btn')
+const darkTheme = 'dark-theme'
+const iconTheme = 'bxs-sun'
 
+// Previously selected topic (if user selected)
+const selectedTheme = localStorage.getItem('selected-theme')
+const selectedIcon = localStorage.getItem('selected-icon')
+
+// We obtain the current theme that the interface has by validating the dark-theme class
+const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light'
+const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'bxs-moon' : 'bxs-sun'
+
+// We validate if the user previously chose a topic
+if (selectedTheme) {
+    // If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the dark
+    document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
+    themeButton.classList[selectedIcon === 'bxs-moon' ? 'add' : 'remove'](iconTheme)
+}
+
+// Activate / deactivate the theme manually with the button
+themeButton.addEventListener('click', () => {
+    // Add or remove the dark / icon theme
+    document.body.classList.toggle(darkTheme)
+    themeButton.classList.toggle(iconTheme)
+    // We save the theme and the current icon that the user chose
+    localStorage.setItem('selected-theme', getCurrentTheme())
+    localStorage.setItem('selected-icon', getCurrentIcon())
+})
 
 /*==================== REDUCE THE SIZE AND PRINT ON AN A4 SHEET ====================*/
 
