9a13dbbb9fc697ed8ce013b2cb00706804bdb302
Cuong Le <cuongedward97@gmail.com>
2022-02-10 19:03:17 +0700

    Final

--- assets/js/main.js
+++ assets/js/main.js
@@ -101,27 +101,55 @@ themeButton.addEventListener('click', () => {
 })
 
 /*==================== REDUCE THE SIZE AND PRINT ON AN A4 SHEET ====================*/
-
+function scaleCv() {
+    document.body.classList.add('scale-cv')
+}
 
 /*==================== REMOVE THE SIZE WHEN THE CV IS DOWNLOADED ====================*/
-
+function removeScaleCv() {
+    document.body.classList.remove('scale-cv')
+}
 
 /*==================== GENERATE PDF ====================*/
 // PDF generated area
+let areaCv = document.getElementById('area-cv')
+let resumeButton = document.getElementById('resume-btn')
 
 
 // Html2pdf options
-
+var opt = {
+    margin: 1,
+    filename: 'Intern-FrontEnd-LeAnhCuong.pdf',
+    image: {
+        type: 'jpeg',
+        quality: 0.98
+    },
+    html2canvas: {
+        scale: 2
+    },
+    jsPDF: {
+        format: 'a4',
+        orientation: 'portrait'
+    }
+}
 
 // Function to call areaCv and Html2Pdf options 
+function generateResume() {
+    html2pdf(areaCv, opt)
+}
 
 
 // When the button is clicked, it executes the three functions
-
-// 1. The class .scale-cv is added to the body, where it reduces the size of the elements
-
-
-// 2. The PDF is generated
+resumeButton.addEventListener('click', () => {
 
 
-// 3. The .scale-cv class is removed from the body after 5 seconds to return to normal size.
+    // 1. The class .scale-cv is added to the body, where it reduces the size of the elements
+    scaleCv()
+
+
+    // 2. The PDF is generated
+
+    generateResume()
+    // 3. The .scale-cv class is removed from the body after 5 seconds to return to normal size.
+    setTimeout(removeScaleCv, 5000)
+})
